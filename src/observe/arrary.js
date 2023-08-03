e19ed86a6e1916b079664d7473fc8532e8dabd4d
yangzi <1131696774@qq.com>
2023-08-03 09:35:44 +0800

    数组方法重写

--- src/observe/arrary.js
+++ src/observe/arrary.js
@@ -1,5 +1,20 @@
-const arrayPrototype = Array.prototype
+export const arrayPrototype = Object.assign(Array.prototype, {})
 
+const arrMethods = [
+  'push',
+  'pop',
+  'shift',
+  'unshift',
+  'splice',
+  'sort',
+  'reverse',
+]
+
+arrMethods.forEach(method => {
+  let origin = arrayPrototype[method]
+  let res = origin.bind(this, arguments)
+  return res
+})
 
 // [
 //   'push',
